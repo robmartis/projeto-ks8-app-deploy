197286c31b26084429478ff714856970917336c3
ROBSON MARTINS <77557379+robmartis@users.noreply.github.com>
2022-12-22 20:01:27 -0300

    app.v.1

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Criando as imagens...."
+
+docker build -t robmartis/projeto-backend:1.0 backend/.
+docker build -t robmartis/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens...."
+
+docker push robmartis/projeto-backend:1.0
+docker push robmartis/projeto-database:1.0
+
+echo " Criando serviços no cluster Kubernetes...."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments...."
+
+kubectl apply -f ./deployment.yml
